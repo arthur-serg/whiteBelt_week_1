91bc8b7e491022b87ca859ab071b61484bbb1edd
arthur-serg <exponenta.euler@gmail.com>
2021-05-07 14:03:41 +0500

    1.Обзор возможностей С++

---
+++ sum.cpp
@@ -0,0 +1,16 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    cout << "Enter a:";
+    cin >> a;
+    cout << "Enter b:";
+    cin >> b;
+    cout << "a+b=" << a + b;
+}
+
